69a6d5daf96a063f69b64320d3e537cabc4a1493
Kayne Ruse <ratstail91@yahoo.com.au>
2013-02-08 09:41:07 +1100

    Created a skeleton for MapUtility, needs testing

--- Emblem of the Lions/main.cpp
+++ Emblem of the Lions/main.cpp
@@ -1,18 +1,50 @@
-#include "region.h"
+#include "map_utility.h"
 
 #include "SDL.h"
 
+#include <stdexcept>
 #include <iostream>
 
 using namespace std;
 
 int SDL_main(int, char**) {
-	Region region;
+/*	Region region;
 
 	region.NewData(40, 40, 3);
 	region.SaveData("region.txt");
 	region.SaveData("region (raw).txt");
 	region.DeleteData();
+*/
+	MapUtility map;
+
+	try {
+		cout << "Creating a new map" << endl;
+		map.NewMap("mapname", 40, 40, 3);
+
+		cout << "Saving the map" << endl;
+		map.SaveMap("mapname");
+
+		cout << "Freeing the existing map" << endl;
+		map.DeleteMap();
+
+		cout << "Loading the map again" << endl;
+		map.LoadMap("mapname");
+
+		cout << "Finally freeing the map again before closing" << endl;
+		map.DeleteMap();
+	}
+	catch(runtime_error& e) {
+		cerr << "Runtime Error: " << e.what() << endl;
+		map.DeleteMap();
+		return -1;
+	}
+	catch (exception& e) {
+		cerr << "Error: " << e.what() << endl;
+		map.DeleteMap();
+		return -2;
+	}
+
+	cout << "Remember to delete the map files before runningthis test again." << endl;
 
 	return 0;
 }
