166e0e1bdafaf1d544f381c83e6fc5acf3387d15
Kayne Ruse <ratstail91@yahoo.com.au>
2013-03-02 16:52:03 +1100

    Work more later

--- Emblem of the Lions/map_persister.h
+++ Emblem of the Lions/map_persister.h
@@ -15,6 +15,8 @@ public:
 	virtual void Delete	(const char* name) = 0;
 	virtual void Unload	() = 0;
 
+	virtual void Cull	(int playerX, int playerY) = 0;
+
 	virtual void DrawTo(SDL_Surface* dest, int camX, int camY) = 0;
 	virtual Tile* SetTile(int x, int y, int z, int v) = 0;
 	virtual Tile* GetTile(int x, int y, int z) = 0;
