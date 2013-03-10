a2fd458953d38a11250780203be2cb0fcd491d22
Kayne Ruse <ratstail91@yahoo.com.au>
2013-03-10 21:08:29 +1100

    Fixed problem, derp

--- execute.cpp
+++ execute.cpp
@@ -24,50 +24,63 @@ int execute(sqlite3* db, const char* command) {
 
 		switch(sqlite3_step(stmt)) {
 			case SQLITE_DONE:
+				cout << "done" << endl;
 			break;
 
 			case SQLITE_ROW:
-				//output the row's data
-				for (int i = 0; i < sqlite3_column_count(stmt); i++) {
-					switch(sqlite3_column_type(stmt, i)) {
-						case SQLITE_INTEGER:
-							cout << sqlite3_column_int(stmt, i);
-						break;
+				do {
+					cout << "row: ";
+					//output the row's data
+					for (int i = 0; i < sqlite3_column_count(stmt); i++) {
+						switch(sqlite3_column_type(stmt, i)) {
+							case SQLITE_INTEGER:
+								cout << "integer: ";
+								cout << sqlite3_column_int(stmt, i);
+							break;
 
-						case SQLITE_FLOAT:
-							cout << sqlite3_column_double(stmt, i);
-						break;
+							case SQLITE_FLOAT:
+								cout << "float: ";
+								cout << sqlite3_column_double(stmt, i);
+							break;
 
-						case SQLITE_TEXT:
-							cout << sqlite3_column_text(stmt, i);
-						break;
+							case SQLITE_TEXT:
+								cout << "text: ";
+								cout << sqlite3_column_text(stmt, i);
+							break;
 
-						case SQLITE_BLOB:
-							cout << "[blob]";
-						break;
+							case SQLITE_BLOB:
+								cout << "blob";
+							break;
 
-						case SQLITE_NULL:
-							cout << "[null]";
-						break;
+							case SQLITE_NULL:
+								cout << "null";
+							break;
 
-						default:
-							cerr << "[unknown type]";
-						break;
+							default:
+								cerr << "unknown type";
+							break;
+						}
 					}
+					cout << endl;
 				}
+				while(sqlite3_step(stmt) == SQLITE_ROW);
 			break;
 
 			case SQLITE_ERROR:
+				cerr << "error" << endl;
 				sqlite3_finalize(stmt);
 				return 1;
 
 			default:
+				cerr << "default error" << endl;
 				sqlite3_finalize(stmt);
 				return 1;
 		}
+		cout << "command compete" << endl;
 		sqlite3_reset(stmt);
 	}
 
+	cout << "input complete" << endl;
 	sqlite3_finalize(stmt);
 	return 0;
 }
@@ -83,15 +96,15 @@ int main(int argc, char* argv[]) {
 
 	const char *command = "\
 	CREATE TABLE IF NOT EXISTS user ('name' TEXT,'age' NUMERIC,'cats' NUMERIC,'programmer' TEXT);\
-	INSERT INTO user VALUES('Kayne', 21, 5, 'true');\
-	INSERT INTO user VALUES('Chris', 23, 1, 'true');\
-	INSERT INTO user VALUES('Logan', 21, 0, 'true');\
-	INSERT INTO user VALUES('Jordan', 17, 5, 'false');\
+	INSERT OR REPLACE INTO user VALUES('Kayne', 21, 5, 'true');\
+	INSERT OR REPLACE INTO user VALUES('Chris', 23, 1, 'true');\
+	INSERT OR REPLACE INTO user VALUES('Logan', 21, 0, 'true');\
+	INSERT OR REPLACE INTO user VALUES('Jordan', 17, 5, 'false');\
 	";
 
 	execute(db, command);
 
-	command = "SELECT * FROM user";
+	command = "SELECT * FROM user;";
 
 	execute(db, command);
 
